e4219bf90f6f535b306cfee5d7889ba1f363606a
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2024-09-21 15:46:41 +0700

    add cascade and specificity

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,1 @@
 import "./style.scss";
-
-const awesome = document.querySelector("[data-element=awesome]");
-const parentBox = document.querySelector('[data-element="parent-box"]');
-
-const intrinsicSwitch = document.querySelector("#intrinsic-switch") as HTMLInputElement | null;
-
-intrinsicSwitch?.addEventListener("change", () => {
-  awesome?.setAttribute("data-sizing", intrinsicSwitch.checked? "intrinsic": "extrinsic");
-  parentBox?.setAttribute("data-sizing", intrinsicSwitch.checked? "intrinsic": "extrinsic");
-});
