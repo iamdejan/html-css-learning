187513a202e643ee106e0f6e3def93c7f4c82e20
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2024-09-22 10:57:25 +0700

    add flexbox with toggle

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 import "./style.scss";
+
+const flexboxToggle = document.querySelector("#flexbox-toggle") as HTMLInputElement|null;
+const root = document.querySelector("div.root");
+
+flexboxToggle?.addEventListener("change", () => {
+  const display = flexboxToggle.checked? "flex": "block";
+  root?.setAttribute("display", display);
+});
