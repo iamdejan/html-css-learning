fcf6a7192b1dd20f6b705d33697ac8aa830820f7
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2024-09-22 11:57:24 +0700

    finish refactor

--- src/flex-direction.ts
+++ src/flex-direction.ts
@@ -2,6 +2,11 @@ const toggle = document.querySelector("#flex-direction-toggle") as HTMLInputElem
 
 toggle?.addEventListener("change", () => {
   const direction = toggle.checked? "column": "row";
-  const root = document.querySelector("div.flex-direction-demo-root");
-  root?.setAttribute("flex-direction", direction);
+  const root = document.querySelector("div.flex-direction-demo-root") as HTMLDivElement|null;
+  root?.style.setProperty("flex-direction", direction);
+
+  const toggleText = document.querySelector("span#direction") as HTMLSpanElement|null;
+  if(toggleText) {
+    toggleText.textContent = direction;
+  }
 });
--- src/flexbox-toggle.ts
+++ src/flexbox-toggle.ts
@@ -2,6 +2,11 @@ const toggle = document.querySelector("#flexbox-toggle") as HTMLInputElement|nul
 
 toggle?.addEventListener("change", () => {
   const display = toggle.checked? "flex": "block";
-  const root = document.querySelector("div.root");
-  root?.setAttribute("display", display);
+  const root = document.querySelector("div.root") as HTMLDivElement|null;
+  root?.style.setProperty("display", display);
+
+  const toggleText = document.querySelector("span#display") as HTMLSpanElement|null;
+  if(toggleText) {
+    toggleText.textContent = display;
+  }
 });
